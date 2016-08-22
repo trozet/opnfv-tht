4e75a189e7082177f473bd8fe8853fd63096bde4
Feng Pan <fpan@redhat.com>
2016-08-22 09:52:32 -0400

    Add nic list and ip address to fdio class (#65)

--- puppet/extraconfig/pre_deploy/controller/manifests/dpdk_dataplanes.pp
+++ puppet/extraconfig/pre_deploy/controller/manifests/dpdk_dataplanes.pp
@@ -34,6 +34,16 @@ $dpdk_tenant_pci_addr = inline_template("<%= `ethtool -i ${dpdk_tenant_port} | g
 
 if ! $dpdk_tenant_pci_addr { fail("Cannot find PCI address of ${dpdk_tenant_port}")}
 
+$dpdk_tenant_port_ip_var = "ipaddress_$dpdk_tenant_port"
+$dpdk_tenant_port_ip = inline_template("<%= scope.lookupvar(dpdk_tenant_port_ip_var) %>")
+if ! $dpdk_tenant_port_ip { fail("Cannot find IP address of ${dpdk_tenant_port}")}
+
+$dpdk_tenant_port_netmask_var = "netmask_$dpdk_tenant_port"
+$dpdk_tenant_port_cidr = inline_template("<%= require 'ipaddr'; IPAddr.new(scope.lookupvar(dpdk_tenant_port_netmask_var)).to_i.to_s(2).count('1') %>")
+if ! $dpdk_tenant_port_cidr { fail("Cannot find cidr of ${dpdk_tenant_port}")}
+
+
+
 if hiera('fdio_enabled', false) {
   file { "vpp dpdk_bind_lock file":
     path   => '/root/dpdk_bind_lock',
@@ -41,6 +51,9 @@ if hiera('fdio_enabled', false) {
   }->
   class { '::fdio::vpp':
     dpdk_pci_devs => [ $dpdk_tenant_pci_addr ],
+    nic_names     => [ $dpdk_tenant_port ],
+    ipaddresses   => [ "${dpdk_tenant_port_ip}/${dpdk_tenant_port_cidr}" ],
+
   }
 
 } else {
